103b44df1958933aafca5331c99bfb2a336db7d1
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-06-12 18:16:15 -0300

    #

--- ejercios_extra/palindromo.js
+++ ejercios_extra/palindromo.js
@@ -1,10 +1,10 @@
 // Realiza un script que pida un texto e indique si es un palíndromo
 
 let input = prompt("Ingrese una palabra o frase para saber si es un palíndromo", "...");
-let inputMinuscula = input.toLowerCase();
-let invertido = Array.from(inputMinuscula).reverse().join("");
+let inputFormateado = input.toLowerCase().replace(/ /g,"");
+let invertido = Array.from(inputFormateado).reverse().join("");
 
-if (inputMinuscula == invertido) {
+if (inputFormateado == invertido) {
     console.log(`El texto ingresado es palíndromo`);
 } else {
     console.log(`el texto ingresado NO es un palíndromo`);
