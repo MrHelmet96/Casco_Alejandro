5c51259ed28d50dcf628950172ffa0b4225fd3c6
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-06-14 20:42:52 -0300

    #

--- modulo05/probandoObjetos.js
+++ modulo05/probandoObjetos.js
@@ -1,21 +1,26 @@
 
 //definición de una clase (estructura o maqueta de futuros objetos)
 class Animal {
+
+    //atributos o caracteristicas del objeto
     nombre;
     color;
     tipo;
+
+    //funcion o método invocado al momento de instaciar una clase, es el encargado de crear o dar existencia a un nuevo objeto de dicha clase
     constructor(name, color, type){
         this.nombre = name;
         this.color = color;
         this.tipo = type;
     }
 
-    //metodos
+    //metodos o comportamientos
     presentarme() {
         console.log(`Hola, soy un ${this.tipo}, me llaman ${this.nombre} y soy de color ${this.color}`);
     }
 }
 
+// variable que será el objeto como tal, de la clase que estoy instanciando
 a1 = new Animal('Olivia', 'gris', 'gato');
 a2 = new Animal('Rufina', 'multicolor', 'gato');
 a3 = new Animal('Treisi', 'negro', 'perro');
