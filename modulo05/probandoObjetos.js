8d6c6cffbb56721f58c05200c7a6d81e24454ba9
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-06-14 20:19:00 -0300

    #

---
+++ modulo05/probandoObjetos.js
@@ -0,0 +1,13 @@
+
+class Animal {
+    nombre;
+    color;
+    constructor(param1, param2){
+        this.nombre = param1;
+        this.color = param2;
+    }
+}
+
+gato = new Animal('Olivia', 'gris');
+
+console.log(gato);
