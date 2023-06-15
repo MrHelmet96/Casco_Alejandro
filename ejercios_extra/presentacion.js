f292887fd42164fcfe181c604aca57111b9e732f
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-06-15 18:18:14 -0300

    #

---
+++ ejercios_extra/presentacion.js
@@ -0,0 +1,11 @@
+let persona = {
+    nombre: 'Alejandro',
+    edad: 27,
+    profesion: 'Programador'
+};
+
+let presentarPersona = function(name, age, profesion) {
+    console.log(`Hola, mi nombre es ${name}, tengo ${age} años y soy ${profesion}`);
+}
+
+console.log(presentarPersona)
