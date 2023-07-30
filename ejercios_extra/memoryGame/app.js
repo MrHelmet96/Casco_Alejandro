543cdeb2d461b6e900113cdd1ae9ad82edd68ddc
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-07-30 19:36:21 -0300

    #

--- ejercios_extra/memoryGame/app.js
+++ ejercios_extra/memoryGame/app.js
@@ -3,7 +3,7 @@ let secondCard = null;
 let canFlip = true;
 
 // Imágenes que se usarán en las cartas
-const images = ["imagen1.jpg", "imagen2.jpg", "imagen3.jpg", "imagen4.jpg", "imagen5.jpg", "imagen6.jpg", /* Agrega aquí más imágenes */];
+const images = ["reverso.jpg", "imagen1.jpg", "imagen2.jpg", "imagen3.jpg", "imagen4.jpg", "imagen5.jpg", "imagen6.jpg" /* Agrega aquí más imágenes */];
 
 // Función para revolver el array de imágenes
 function shuffleImages(array) {
@@ -12,10 +12,10 @@ function shuffleImages(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
 
-  // Agregar evento click a cada carta después de crearlas en el DOM
-  const cards = document.querySelectorAll(".card");
-  cards.forEach((card, index) => {
-    card.addEventListener("click", () => flipCard(index));
+  // Agregar evento click a cada imagen dentro de las cartas después de crearlas en el DOM
+  const imagesElements = document.querySelectorAll(".card img.front");
+  imagesElements.forEach((image, index) => {
+    image.addEventListener("click", () => flipCard(index));
   });
 }
 
@@ -23,8 +23,9 @@ function shuffleImages(array) {
 function flipCard(index) {
   if (!canFlip) return;
 
-  const card = document.getElementById("card" + index);
-  card.classList.toggle("flipped");
+  const frontId = "front" + index;
+  const card = document.getElementById(frontId);
+  card.parentElement.classList.toggle("flipped");
 
   if (firstCard === null) {
     firstCard = card;
@@ -40,15 +41,15 @@ function checkCards() {
 
   setTimeout(() => {
     if (firstCard.src === secondCard.src) {
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
+      firstCard.parentElement.removeEventListener("click", flipCard);
+      secondCard.parentElement.removeEventListener("click", flipCard);
       firstCard = null;
       secondCard = null;
       canFlip = true;
       checkWin();
     } else {
-      firstCard.classList.remove("flipped");
-      secondCard.classList.remove("flipped");
+      firstCard.parentElement.classList.remove("flipped");
+      secondCard.parentElement.classList.remove("flipped");
       firstCard = null;
       secondCard = null;
       canFlip = true;
