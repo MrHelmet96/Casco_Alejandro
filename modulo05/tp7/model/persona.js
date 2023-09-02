0e1fc98ee7812a0d695e8afbfa590dbcf52d245f
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-09-02 19:23:13 -0300

    #

--- modulo05/tp7/model/persona.js
+++ modulo05/tp7/model/persona.js
@@ -1,41 +1,22 @@
 require('rootpath')();
-
-var persona_db = {};
-
 const { query } = require('express');
 const mysql = require('mysql');
 const configuracion = require("config.json");
 
+var UconsultaAbd = {};
+var conexion = mysql.createConnection(configuracion.database);
 
-var connection = mysql.createConnection(configuracion.database);
-connection.connect((err) => {
+conexion.connect((err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log("base de datos conectada");
+        console.log("base de datos de usuario enlazada");
     }
 });
 
-
-
-//-- antes -------------------------------------------------------------------
-//teniamos un solo archivo que era el index //codificaba todo en la misma funcion
-
-
-//-- ahora -------------------------------------------------------------------
-//tenemos 2 archivos que son el persona_index y la persona_BD
-//tengo que codificar en dos funciones y comunicarme entre ellas
-//persona_index (interaccion con el servidor): se encargara de mandarle los mensajes al frontend y de hacer peticiones a persona_BD  
-//persona_BD (interaccion con la base de datos): recibira peticiones de persona_index y debera devolver una respuesta
-//¿como me comunico?: una forma invocar(mandar una funcion [carretilla vacia]) ---> atender (recibir la funcion que me mandaron)
-//persona_BD are lo que tenga que hacer y enviare mis datos a la funcion que me enviaron [llenar la carretilla]
-
-
-
-
-persona_db.getAll = function (funCallback) {
-    var consulta = 'SELECT * FROM persona';
-    connection.query(consulta, function (err, rows) {
+UconsultaAbd.getAll = function (funCallback) {
+    var consulta = 'SELECT * FROM usuario';
+    conexion.query(consulta, function (err, rows) {
         if (err) {
             funCallback(err);
             return;
@@ -45,124 +26,72 @@ persona_db.getAll = function (funCallback) {
     });
 }
 
-persona_db.getByDNI = function (dni, funCallback) {
-    connection.query("select * from persona where dni = ?;", dni, (err, respuesta) => {
-        if (err) {
-            funCallback({
-                mensajito: "a ocurrido algun error inesperado al buscar la persona",
-                detalle: err
-            });
-        } else if (respuesta.length == 0) { //consulta no impacta en nada dentro de la BD
-            funCallback(undefined, {
-                mensajito: `no se encontro la persona con el dni: ${dni}`,
-                detalle: respuesta
-            });
-        }else {
+UconsultaAbd.createUser = function (usuario, funcallback) {
+    consulta = "INSERT INTO usuario (mail, nickname, clave, persona) VALUES (?,?,?,?);";
+    params = [usuario.mail, usuario.nickname, usuario.clave, usuario.persona];
 
-            funCallback(undefined, {
-                mensajito: `los datos de la persona con el dni ${dni} son:`,
-                detalle: detail_bd
-            });
-        }
-    });
-    
-}
-
-
-persona_db.create = function (persona, funcallback) {
-    consulta = "INSERT INTO persona (dni, nombre, apellido) VALUES (?,?,?);";
-    params = [persona.dni, persona.nombre, persona.apellido];
-
-    connection.query(consulta, params, (err, detail_bd) => {
+    conexion.query(consulta, params, (err, detail_bd) => {
         if (err) {
 
             if (err.code == "ER_DUP_ENTRY") {
                 funcallback({
-                    mensajito: "La persona ya fue registrada",
-                    detalle: err
+                    mensajito: "Ya se ah registrado este usuario con el nickname: " + usuario.nickname,
                 });
             } else {
                 funcallback({
-                    mensajito: "error diferente",
+                    mensajito: "Error encontrado",
                     detalle: err
                 });
             }
         } else {
-
             funcallback(undefined, {
-                mensajito: "se creo la persona " + persona.nombre + persona.apellido,
+                mensajito: "Se creo el usuario: " + usuario.nickname,
                 detalle: detail_bd
             });
         }
     });
 }
 
+UconsultaAbd.modificar = function(usuario, funcallback) {
+    const consulta = "UPDATE usuario SET nickname = ?, clave = ? WHERE mail = ?";
+    const params = [usuario.nickname, usuario.clave, usuario.mail];
 
-persona_db.borrar = function (id_p_e, retorno) {
-    consulta = "DELETE FROM persona WHERE dni = ?";
+    conexion.query(consulta, params, (err, resultado) => {
+        if (err) {
+            funcallback(err);
+        } else {
+            funcallback(null, resultado);
+        }
+    });
+};
+
+UconsultaAbd.borrar = function (id_p_e, retorno) { 
+    consulta = "DELETE FROM usuario WHERE mail = ?";
     parametro = id_p_e;
 
-    connection.query(consulta, parametro, (err, result) => {
-        if (err) {
-            retorno({ menssage: err.code, detail: err }, undefined);
-
+    conexion.query(consulta, parametro, (err, result) => {
+        if(err) {
+            retorno({message: err.code, detail: err}, undefined);
+        }else{ if (result.affectedRows == 0) {
+            retorno(undefined, { mensajito: "No se encontro al usuario con el mail " + id_p_e});
         } else {
+            retorno(undefined, { mensajito: "Usuario eliminado con el mail: "+ id_p_e });
+        }
+    }
+    })
+};
 
-            if (result.affectedRows == 0) {
-                retorno(undefined, { message: "no se encontro la persona, ingrese otro DNI", detail: result });
-            } else {
-                retorno(undefined, { message: "persona eliminada", detail: result });
-            }
+UconsultaAbd.getByEmail = function(mail, funcallback) {
+    const consulta = "SELECT mail FROM usuario";
+    const params = mail;
+
+    conexion.query(consulta, params, (err, resultado) => {
+        if (err) {
+            funcallback(err);
+        } else {
+            funcallback(null, resultado);
         }
     });
-
-    //borrara la persona y le comunicara al conrtrolador como salio la cosa
-
-}
-
-
-persona_db.getUserByPersona = function (dni, funcallback) {
-
-    connection.query("select * from persona where dni = ?", dni, (err, respuesta) => {
-        if (err) {
-            funcallback({
-                mensajito: "a ocurrido algun error, posiblemente de sintaxis en buscar la persona",
-                detalle: err
-            });
-        } else if (respuesta.length == 0) { //consulta no impacta en nada dentro de la BD
-            funcallback(undefined, {
-                mensajito: "no se encontro la persona buscada",
-                detalle: respuesta
-            });
-        } else {
-            consulta = "select nickname from usuario INNER JOIN persona on usuario.persona = persona.dni and usuario.persona = ?";
-            connection.query(consulta, dni, (err, r) => {
-                if (err) {
-                    funcallback({
-                        mensajito: "a ocurrido algun error, posiblemente de sintaxis en buscar el nickname",
-                        detalle: err
-                    });
-                } else if (r.length == 0) { //array vacio
-                    funcallback(undefined, {
-                        mensajito: "la persona seleccionada no posee usuario registrado en la base de datos",
-                        detalle: r
-                    });
-                } else {
-                    funcallback(undefined, { // consulta impacta bien, y el array no esta vacio 
-                        mensajito: `El nikname de la persona seleccionada es ${r[0]['nickname']}`,
-                        detalle: r
-                    });
-                }
-            });
-
-
-        }
-    });
-
-
-
-
-}
-
+};
 
 module.exports = persona_db;
