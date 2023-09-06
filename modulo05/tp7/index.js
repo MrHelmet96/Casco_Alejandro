c2139a409e6ec71fb7b01b9399de4a736572456d
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2023-09-06 17:08:07 -0300

    #

--- modulo05/tp7/index.js
+++ modulo05/tp7/index.js
@@ -2,6 +2,7 @@ require('rootpath')();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
@@ -11,12 +12,10 @@ const configuracion = require("config.json");
 const controladorPersona = require("controller/personaController.js");
 const controladorUsuario = require("controller/usuarioController.js");
 
-
 app.use('/api/persona', controladorPersona);
 app.use('/api/usuario', controladorUsuario);
 
-
-app.listen(3000, (err) => {
+app.listen(configuracion.server.port, (err) => {
     if (err) {
         console.log(err);
     } else {
